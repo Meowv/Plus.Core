baf52e6fc29717c7fbfc463bfa1d5b1cb5632f25
阿星Plus <hackxing@foxmail.com>
2020-08-03 17:16:58 +0800

    Autofac EventBus

---
+++ ConsoleApp1/TestEventData.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    public class TestEventData
+    {
+        public TestEventData(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+    }
+}
