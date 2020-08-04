02834f108202e0623ac8142a5f76f9dfccb2249e
阿星Plus <hackxing@foxmail.com>
2020-08-04 16:11:44 +0800

    Linq

---
+++ Plus.Core/Plus/Linq/PlusPagingQueryableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Plus.Application.Dtos;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Plus.Linq
+{
+    public static class PlusPagingQueryableExtensions
+    {
+        /// <summary>
+        /// Used for paging with an <see cref="IPagedResultRequest"/> object.
+        /// </summary>
+        /// <param name="query">Queryable to apply paging</param>
+        /// <param name="pagedResultRequest">An object implements <see cref="IPagedResultRequest"/> interface</param>
+        public static IQueryable<T> PageBy<T>([NotNull] this IQueryable<T> query, IPagedResultRequest pagedResultRequest)
+        {
+            Check.NotNull(query, nameof(query));
+
+            return query.PageBy(pagedResultRequest.SkipCount, pagedResultRequest.MaxResultCount);
+        }
+    }
+}
