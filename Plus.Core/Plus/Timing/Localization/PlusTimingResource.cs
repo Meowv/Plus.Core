302ed90d5f9113a3ce98a171e5cf7e677a8dab71
阿星Plus <hackxing@foxmail.com>
2020-08-04 15:09:01 +0800

    Timing Localization

---
+++ Plus.Core/Plus/Timing/Localization/PlusTimingResource.cs
@@ -0,0 +1,10 @@
+﻿using Plus.Localization;
+
+namespace Plus.Timing.Localization.Resources.PlusTiming
+{
+    [LocalizationResourceName("PlusTiming")]
+    public class PlusTimingResource
+    {
+
+    }
+}
---
+++ Plus.Core/Plus/Timing/TimingSettingProvider.cs
@@ -0,0 +1,25 @@
+﻿using Plus.Localization;
+using Plus.Settings;
+using Plus.Timing.Localization.Resources.PlusTiming;
+
+namespace Plus.Timing
+{
+    public class TimingSettingProvider : SettingDefinitionProvider
+    {
+        public override void Define(ISettingDefinitionContext context)
+        {
+            context.Add(
+                new SettingDefinition(TimingSettingNames.TimeZone,
+                    "UTC",
+                    L("DisplayName:Plus.Timing.Timezone"),
+                    L("Description:Plus.Timing.Timezone"),
+                    isVisibleToClients: true)
+            );
+        }
+
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<PlusTimingResource>(name);
+        }
+    }
+}
