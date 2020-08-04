1f92fa81914e46e3d4f2f8c670dbbd4359d92935
阿星Plus <hackxing@foxmail.com>
2020-08-04 15:11:48 +0800

    Serialization

---
+++ Plus.Core/Plus/Serialization/PlusSerializationModule.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Plus.Modularity;
+using Plus.Reflection;
+
+namespace Plus.Serialization
+{
+    public class PlusSerializationModule : PlusModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.OnExposing(onServiceExposingContext =>
+            {
+                //Register types for IObjectSerializer<T> if implements
+                onServiceExposingContext.ExposedTypes.AddRange(
+                    ReflectionHelper.GetImplementedGenericTypes(
+                        onServiceExposingContext.ImplementationType,
+                        typeof(IObjectSerializer<>)
+                    )
+                );
+            });
+        }
+    }
+}
