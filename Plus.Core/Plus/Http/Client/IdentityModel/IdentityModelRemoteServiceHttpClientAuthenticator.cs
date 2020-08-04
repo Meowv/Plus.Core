f982c4bd80141bd1f98170dee888f0a2f0d5bbdb
阿星Plus <hackxing@foxmail.com>
2020-08-04 17:00:11 +0800

    IdentityModel

---
+++ Plus.Core/Plus/Http/Client/IdentityModel/IdentityModelRemoteServiceHttpClientAuthenticator.cs
@@ -0,0 +1,27 @@
+﻿using Plus.DependencyInjection;
+using Plus.Http.Client.Authentication;
+using Plus.IdentityModel;
+using System.Threading.Tasks;
+
+namespace Plus.Http.Client.IdentityModel
+{
+    [Dependency(ReplaceServices = true)]
+    public class IdentityModelRemoteServiceHttpClientAuthenticator : IRemoteServiceHttpClientAuthenticator, ITransientDependency
+    {
+        protected IIdentityModelAuthenticationService IdentityModelAuthenticationService { get; }
+
+        public IdentityModelRemoteServiceHttpClientAuthenticator(
+            IIdentityModelAuthenticationService identityModelAuthenticationService)
+        {
+            IdentityModelAuthenticationService = identityModelAuthenticationService;
+        }
+
+        public virtual async Task AuthenticateAsync(RemoteServiceHttpClientAuthenticateContext context)
+        {
+            await IdentityModelAuthenticationService.TryAuthenticateAsync(
+                context.Client,
+                context.RemoteService.GetIdentityClient()
+            );
+        }
+    }
+}
---
+++ Plus.Core/Plus/Http/Client/IdentityModel/PlusHttpClientIdentityModelModule.cs
@@ -0,0 +1,14 @@
+﻿using Plus.IdentityModel;
+using Plus.Modularity;
+
+namespace Plus.Http.Client.IdentityModel
+{
+    [DependsOn(
+        typeof(PlusHttpClientModule),
+        typeof(PlusIdentityModelModule)
+        )]
+    public class PlusHttpClientIdentityModelModule : PlusModule
+    {
+
+    }
+}
