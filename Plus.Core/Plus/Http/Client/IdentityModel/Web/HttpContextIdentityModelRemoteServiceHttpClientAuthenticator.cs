986becbea98804ffcc124ad45d3ac346ca13f4b3
阿星Plus <hackxing@foxmail.com>
2020-08-05 13:44:57 +0800

    Http.Client.IdentityModel

---
+++ Plus.Core/Plus/Http/Client/IdentityModel/Web/HttpContextIdentityModelRemoteServiceHttpClientAuthenticator.cs
@@ -0,0 +1,52 @@
+﻿#if NETCOREAPP3_1
+
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Plus.DependencyInjection;
+using Plus.Http.Client.Authentication;
+using Plus.IdentityModel;
+using System.Threading.Tasks;
+
+namespace Plus.Http.Client.IdentityModel.Web
+{
+    [Dependency(ReplaceServices = true)]
+    public class HttpContextIdentityModelRemoteServiceHttpClientAuthenticator : IdentityModelRemoteServiceHttpClientAuthenticator
+    {
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public HttpContextIdentityModelRemoteServiceHttpClientAuthenticator(
+            IIdentityModelAuthenticationService identityModelAuthenticationService)
+            : base(identityModelAuthenticationService)
+        {
+        }
+
+        public override async Task AuthenticateAsync(RemoteServiceHttpClientAuthenticateContext context)
+        {
+            if (context.RemoteService.GetUseCurrentAccessToken() != false)
+            {
+                var accessToken = await GetAccessTokenFromHttpContextOrNullAsync();
+                if (accessToken != null)
+                {
+                    context.Request.SetBearerToken(accessToken);
+                    return;
+                }
+            }
+
+            await base.AuthenticateAsync(context);
+        }
+
+        protected virtual async Task<string> GetAccessTokenFromHttpContextOrNullAsync()
+        {
+            var httpContext = HttpContextAccessor?.HttpContext;
+            if (httpContext == null)
+            {
+                return null;
+            }
+
+            return await httpContext.GetTokenAsync("access_token");
+        }
+    }
+}
+
+#endif
---
+++ Plus.Core/Plus/Http/Client/IdentityModel/Web/PlusHttpClientIdentityModelWebModule.cs
@@ -0,0 +1,12 @@
+﻿using Plus.Modularity;
+
+namespace Plus.Http.Client.IdentityModel.Web
+{
+    [DependsOn(
+        typeof(PlusHttpClientIdentityModelModule)
+        )]
+    public class PlusHttpClientIdentityModelWebModule : PlusModule
+    {
+
+    }
+}
