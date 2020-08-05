2624b77c34bd8805caea38b611470e3291148bfb
阿星Plus <hackxing@foxmail.com>
2020-08-05 14:14:03 +0800

    Plus.AspNetCore.Authentication.OAuth.Tests

---
+++ test/Plus.AspNetCore.Authentication.OAuth.Tests/Plus/AspNetCore/Authentication/OAuth/Claims/MultipleClaimAction_Tests.cs
@@ -0,0 +1,45 @@
+﻿using Plus.Security.Claims;
+using Shouldly;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+using Xunit;
+
+namespace Plus.AspNetCore.Authentication.OAuth.Claims
+{
+    public class MultipleClaimAction_Tests
+    {
+        [Fact]
+        public void Should_Set_Single_Value()
+        {
+            var jObject = JsonDocument.Parse(@"{
+  ""sub"": ""71054539-0e48-af28-5e7a-39e4e42d8ea5"",
+  ""role"": ""admin""
+}");
+            var claimsIdentity = new ClaimsIdentity();
+            new MultipleClaimAction(PlusClaimTypes.Role, "role").Run(jObject.RootElement, claimsIdentity, null);
+            var claims = claimsIdentity.FindAll(PlusClaimTypes.Role).ToList();
+            claims.Count.ShouldBe(1);
+            claims[0].Value.ShouldBe("admin");
+        }
+
+        [Fact]
+        public void Should_Set_Multiple_Values()
+        {
+            var jObject = JsonDocument.Parse(@"{
+  ""sub"": ""71054539-0e48-af28-5e7a-39e4e42d8ea5"",
+  ""role"": [
+    ""admin"",
+    ""moderator""
+  ]
+}");
+
+            var claimsIdentity = new ClaimsIdentity();
+            new MultipleClaimAction(PlusClaimTypes.Role, "role").Run(jObject.RootElement, claimsIdentity, null);
+            var claims = claimsIdentity.FindAll(PlusClaimTypes.Role).ToList();
+            claims.Count.ShouldBe(2);
+            claims[0].Value.ShouldBe("admin");
+            claims[1].Value.ShouldBe("moderator");
+        }
+    }
+}
